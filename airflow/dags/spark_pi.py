a440934c8fa445305d10b841ec29b3adff420ac8
Viet Hoang <hoangquocvietbk2.13@gmail.com>
2024-04-11 13:08:31 +0700

    add airflow spark dags

--- airflow/dags/spark_pi.py
+++ airflow/dags/spark_pi.py
@@ -30,29 +30,26 @@ default_args = {
 }
 # [END default_args]
 
-# [START instantiate_dag]
 dag = DAG(
-    "spark_pi_1",
+    "spark_pi",
     default_args=default_args,
-    schedule_interval=timedelta(days=1),
-    tags=["example"],
+    schedule_interval=None,
+    tags=["example", "spark"],
 )
-
-# spark = open(
-#     "example_spark_kubernetes_operator_pi.yaml").read()
 
 submit = SparkKubernetesOperator(
     task_id="spark_pi_submit",
     namespace="spark-operator",
-    application_file="./spark_yaml/spark_pi.yaml",
+    application_file="spark_yaml/spark_pi.yaml",
     kubernetes_conn_id="kubernetes_default",
     do_xcom_push=True,
     dag=dag,
+    enable_impersonation_from_ldap_user=False,
 )
 
 sensor = SparkKubernetesSensor(
     task_id="spark_pi_monitor",
-    namespace=" spark-operator",
+    namespace="spark-operator",
     application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
     kubernetes_conn_id="kubernetes_default",
     dag=dag,
