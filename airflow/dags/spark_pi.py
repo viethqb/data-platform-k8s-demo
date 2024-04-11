d21b9e88898de0046a6fbe4c5dfbeb826c4743a6
Viet Hoang <hoangquocvietbk2.13@gmail.com>
2024-04-11 12:46:30 +0700

    add airflow spark dags

--- airflow/dags/spark_pi.py
+++ airflow/dags/spark_pi.py
@@ -1,3 +1,36 @@
+from datetime import timedelta, datetime
+
+# [START import_module]
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+
+# Operators; we need this to operate!
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import (
+    SparkKubernetesOperator,
+)
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import (
+    SparkKubernetesSensor,
+)
+from airflow.utils.dates import days_ago
+
+# [END import_module]
+
+# [START default_args]
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "max_active_runs": 1,
+    "retries": 3,
+}
+# [END default_args]
+
+# [START instantiate_dag]
 dag = DAG(
     "spark_pi",
     default_args=default_args,
