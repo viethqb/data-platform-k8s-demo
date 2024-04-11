3f218659e979ed3802f098bcb19a2efd29ddc7ec
Viet Hoang <hoangquocvietbk2.13@gmail.com>
2024-04-11 12:51:32 +0700

    add airflow spark dags

--- airflow/dags/spark_pi.py
+++ airflow/dags/spark_pi.py
@@ -32,7 +32,7 @@ default_args = {
 
 # [START instantiate_dag]
 dag = DAG(
-    "spark_pi",
+    "spark_pi_1",
     default_args=default_args,
     schedule_interval=timedelta(days=1),
     tags=["example"],
